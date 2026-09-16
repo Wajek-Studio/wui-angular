f587816849b6a513b62b65a81f8b1d2355fa99b4
mochrira <mochrira@gmail.com>
2026-09-16 14:01:36 +0700

    refactor tipografi demo component

--- src/app/pages/tipografi.page/tipografi.page.ts
+++ src/app/pages/tipografi.page/tipografi.page.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, TemplateRef, inject, viewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { WuiPage, WuiPageService, WuiSidenav } from '@wajek/wui';
+import { WuiPage, WuiPageService } from '@wajek/wui';
 
 @Component({
   selector: 'app-tipografi.page',
-  imports: [WuiPage, RouterLink, WuiSidenav],
+  imports: [WuiPage, RouterLink],
   templateUrl: './tipografi.page.html',
   styleUrl: './tipografi.page.scss',
 })
