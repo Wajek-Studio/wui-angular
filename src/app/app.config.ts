58fcf986c6fd56070dc7ef4424cbcc0d116c7b71
Kusen Abdullah <kusena322@gmail.com>
2026-09-17 11:58:20 +0700

    docs dialog

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,6 +14,7 @@ import {
   mdiHome,
   mdiMenu,
   mdiTable,
+  mdiWindowMaximize,
 } from '@mdi/js';
 
 export const appConfig: ApplicationConfig = {
@@ -31,7 +32,8 @@ export const appConfig: ApplicationConfig = {
       { name: 'gesture-tap-button', path: mdiGestureTapButton },
       { name: 'dock-left', path: mdiDockLeft },
       { name: 'table', path: mdiTable },
-      { name: 'dots-vertical', path: mdiDotsVertical }
+      { name: 'dots-vertical', path: mdiDotsVertical },
+      { name: 'dialog', path: mdiWindowMaximize }
     ),
   ],
 };
